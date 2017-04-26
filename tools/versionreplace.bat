c2fbd656b867199405800e5edf6b07d347ab288f
Troels Liebe Bentsen <trbe@danskebank.com>
2017-04-26 16:21:57 +0200

    Add more debug info

--- tools/versionreplace.bat
+++ tools/versionreplace.bat
@@ -1,4 +1,6 @@
 echo off
 set csprojfile=%1
 set buildnumber=%2
+echo Setting buildnumber %buildnumber%
 %~dp0sed.exe -i -E "s/(<Version>[0-9]\.[0-9]\.[0-9]\.)[0-9](<\/Version>)/\1%buildnumber%\2/" %csprojfile%
+type %csprojfile%
