95a80aeab9ba711fea1fca81dc11fca4289c96af
Troels Liebe Bentsen <trbe@danskebank.com>
2017-04-26 17:07:39 +0200

    Use git commit count as build version

--- tools/versionreplace.bat
+++ tools/versionreplace.bat
@@ -1,6 +1,11 @@
 echo off
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list HEAD --count`) DO (
+SET buildnumber=%%F
+)
+
 set csprojfile=%1
-set buildnumber=%2
+
 echo Setting buildnumber %buildnumber%
 %~dp0sed.exe -i -E "s/(<Version>[0-9]\.[0-9]\.[0-9]\.)[0-9](<\/Version>)/\1%buildnumber%\2/" %csprojfile%
 type %csprojfile%
